54c224764cc99438a64f1cfd2f466d0f69139a3a
hoit1302 <jueun000101@gmail.com>
2020-11-21 11:25:00 +0900

    1.1 Securing the Keys

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,16 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC9G66G3FdMgh4rFTnKmOAyuqDQYwQP8sI",
-    authDomain: "nwitter-5bfcd.firebaseapp.com",
-    databaseURL: "https://nwitter-5bfcd.firebaseio.com",
-    projectId: "nwitter-5bfcd",
-    storageBucket: "nwitter-5bfcd.appspot.com",
-    messagingSenderId: "711658436726",
-    appId: "1:711658436726:web:65b55861eddfc9b6218757"
+    // 키 값을 직접 써주지 않는 이유는 키 들을 github에 올리고 싶지 않아서이다.
+    // 하지만 빌드를 하면 값이 바뀌기 때문에 결국엔 사용자에게 보인다.
+    // 이 부분은 결국 github만을 위한 코드
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase

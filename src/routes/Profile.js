37dd16c33fe03843ba3dc0c3bed4586d05e38ac1
hoit1302 <jueun000101@gmail.com>
2020-11-21 21:41:07 +0900

    2.6 Log Out

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,17 @@
 import React from "react";
+import { authService } from "fbase";
+import { useHistory } from "react-router-dom";
 
-export default () => <span>Profile</span>;
+export default () => {
+    // https://reactrouter.com/web/api/Hooks/usehistory
+    const history = useHistory();
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/"); // logout 후 다시 home으로 돌아갈 수 있도록 url 바꿔 redirect
+    };
+    return (
+        <>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    );
+};

cc3b2dfcc8389e4bbc3421931294ed0e03c77775
hoit1302 <jueun000101@gmail.com>
2020-11-21 21:39:23 +0900

    2.6 Log Out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,22 +1,31 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />} 
+            {/* Navigation이 존재하려면, isLoggedIn이 true 값이여야 한다는 뜻 */}
             <Switch>
                 {isLoggedIn ? (
                     <>
                         <Route exact path="/">
                             <Home />
                         </Route>
+                        <Route exact path="/profile">
+                            <Profile />
+                        </Route>
                     </>
                 ) : (
-                        <Route exact path="/">
-                            <Auth />
-                        </Route>
+                        <>
+                            <Route exact path="/">
+                                <Auth />
+                            </Route>
+                        </>
                     )}
             </Switch>
         </Router>

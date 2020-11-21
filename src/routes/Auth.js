53c39b60d8c8410f427665ab364889c3e0c1e31a
hoit1302 <jueun000101@gmail.com>
2020-11-21 18:43:57 +0900

    2.3 Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import { authService } from "fbase";
 
+// =>: arrow function. 모던 자바스크립의 종류. 이것 자체에 return이라는 뜻이 내재되어 있기에 return을 표시하지 않아도 된다.
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
     const onChange = (event) => {
         // event로부터 정보를 받아 옴. 그 중 하나의 정보가 target이고 target안에 들어있는 name과 value를 가져 옴
         const { target: { name, value }, } = event;
@@ -12,10 +15,27 @@ const Auth = () => {
             setPassword(value); // input의 value는 state에 저장된다.
         }
     };
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         // submit시 default로 refresh 되는 것 방지.
         event.preventDefault(); // prevent default event
+
+        try {
+            // https://firebase.google.com/docs/reference/js/firebase.auth.EmailAuthProvider
+            let data;
+            if (newAccount) {
+                // create
+                // await (promise라서)은 async를 써야 작동한다.
+                data = await authService.createUserWithEmailAndPassword(email, password);
+            } else {
+                // log in
+                data = await authService.signInWithEmailAndPassword(email, password);
+            }
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
     };
+
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -23,7 +43,7 @@ const Auth = () => {
                     value={email} onChange={onChange} />
                 <input name="password" type="password" placeholder="Password" required
                     value={password} onChange={onChange} />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
             </form>
             <div>
                 <button>Continue with Google</button>
